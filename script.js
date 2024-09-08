c247a6a7ec599914de0c3ab2d03b63147854e124
Thomas Jones <Thomasajones@hotmail.co.uk>
2024-09-08 22:14:55 +0100

    Version 1.0

--- script.js
+++ script.js
@@ -1,67 +1,65 @@
-// Tags being displayed per page
-const tagsPerPage = 20;
+const pages = 20;
 
-// Event for uploading
 document
   .getElementById("uploadButtonID")
   .addEventListener("click", async () => {
-    // Handling the files
+    // ID Elements and file handlers
     const fileInput = document.getElementById("imageInputID");
     const file = fileInput.files[0];
     const imagePreview = document.getElementById("imagePreviewID");
     const uploadModal = document.getElementById("uploadModalID");
     const uploadProgress = document.getElementById("uploadProgressID");
 
-    // No file image has been selected
-    if (!file) return showToast("Please select an image file.");
+    // If no file has been selected, show the message
+    if (!file) return showToast("Please select an image file first");
 
-    // Preview the image
+    //Prview the selected images
     const reader = new FileReader();
     reader.onload = (e) => (imagePreview.src = e.target.result);
     reader.readAsDataURL(file);
 
-    // Imagga API Code
+    // Imagga API access keys and credentials
     const apiKey = "acc_bce9a07966884cb";
     const apiSecret = "7a5e0f654356a020f31ca4b9c729e830";
     const authHeader = "Basic " + btoa(`${apiKey}:${apiSecret}`);
 
-    // API Data for uploaded images
+    // Preparing the data for upload
     const formData = new FormData();
     formData.append("image", file);
 
     try {
-      // Show the uploaded modal and loading bar when getting image data
+      // Show the upload modal when fetching the data and dispay progress bar
       uploadModal.style.display = "block";
       uploadProgress.style.width = "0%";
 
-      // When the image is sent to Imagga
-      const uploadRespond = await fetch("https://api.imagga.com/v2/uploads", {
+      // Upload image to Imagga API
+      const uploadResponse = await fetch("https://api.imagga.com/v2/uploads", {
         method: "POST",
-        headers: { Authorization: authHeader },
+        headers: { authorization: authHeader },
         body: formData,
       });
 
-      if (!uploadRespond.ok) throw new Error("Image Upload Failed.");
+      if (!uploadResponse.ok) throw new Error("Upload failed.");
 
-      // Tracking the upload progress
-      const contentLength = +uploadRespond.headers.get("Content-Length");
-      const reader = uploadRespond.body.getReader();
-
+      // Track upload progress
+      const contentLength = +uploadResponse.headers.get("Content-Length");
+      const reader = uploadResponse.body.getReader();
       let receivedLength = 0;
       let chunks = [];
 
-      // Read the response and the updated progress on the image
+      // Read response stream and update progress bar
       while (true) {
         const { done, value } = await reader.read();
         if (done) break;
         chunks.push(value);
         receivedLength += value.length;
-        uploadProgress.style.width`${(receivedLength / contentLength) * 100}%`;
+        uploadProgress.style.width = `${
+          (receivedLength / contentLength) * 100
+        }%`;
       }
 
-      // The decoded and parsed upload response
+      // Decode and parse upload response
       const responseArray = new Uint8Array(receivedLength);
-
       let position = 0;
       for (const chunk of chunks) {
         responseArray.set(chunk, position);
@@ -73,30 +71,29 @@ document
         result: { upload_id },
       } = JSON.parse(text);
 
-      // fetch colors and tag data from analysis and Imagga API
-      const { colorResult, tagsResult } = await Promise.all([
+      // Fetch color and tag analysis from the Imagga API
+      const [colorResult, tagsResult] = await Promise.all([
         fetch(`https://api.imagga.com/v2/colors?image_upload_id=${upload_id}`, {
           headers: { Authorization: authHeader },
         }).then((res) => res.json()),
-
         fetch(`https://api.imagga.com/v2/tags?image_upload_id=${upload_id}`, {
           headers: { Authorization: authHeader },
         }).then((res) => res.json()),
       ]);
 
-      // Display the results in the UI
+      // Display the results
       displayColors(colorResult.result.colors);
       displayTags(tagsResult.result.tags);
     } catch (error) {
-      console.error("Error", error);
-      showToast("An error has occured while uploading image.");
+      console.error("Error:", error);
+      showToast("An error has occured while processing the image.");
     } finally {
-      // Hide the upload loading bar after upload has been complete
+      // Hide the upload modal after the processsing is completed
       uploadModal.style.display = "none";
     }
   });
 
-// Function to display the colors analysis results.
+// function to display color analysis results.
 const displayColors = (colors) => {
   const colorsContainer = document.querySelector(".AI-colors-container");
   colorsContainer.innerHTML = ``;
@@ -116,28 +113,28 @@ const displayColors = (colors) => {
   // Generate the HTML code section for the different color types found in the image
   const generateColorSection = (title, colorData) => {
     return `
-    <h3>${title}</h3>
-    <div class="results">
-    ${colorData
-      .map(
-        ({ html_code, closest_palette_color, percent }) =>
-          `
-      <div class="result-item" data-color"${html_code}">
-      <div>
-      <div class="color-box" style="background-color: ${html_code}" title="Color code: ${html_code}"></div>
-      <p>${html_code}<span> - ${closest_palette_color}</span></p>
-      </div>
-
-      <div class="progress-bar">
-      <span>${percent.toFixed(2)}%</span>
-      <div class="progress" style="width: ${percent}%></div>
-      </div>
-      </div>
-`
-      )
-      .join("")}
-      </div>
-    `;
+      <h3>${title}</h3>
+      <div class="results">
+      ${colorData
+        .map(
+          ({ html_code, closest_palette_color, percent }) =>
+            `
+        <div class="result-item" data-color"${html_code}">
+        <div>
+        <div class="color-box" style="background-color: ${html_code}" title="Color code: ${html_code}">
+        <p>${html_code}<span> - ${closest_palette_color}</span></p>
+        </div>
+  
+        <div class="progress-bar">
+        <span>${percent.toFixed(2)}%</span>
+        <div class="progress" style="width: ${percent}%></div>
+        </div>
+        </div>
+  `
+        )
+        .join("")}
+        </div>
+      `;
   };
 
   // Append and generate the color section with IMAGGA data to the container
@@ -159,7 +156,7 @@ const displayColors = (colors) => {
     .querySelectorAll(".AI-colors-container .result-item")
     .forEach((item) => {
       item.addEventListener("click", () => {
-        const colorCode = item.getAttribute("data-color");
+        const colorCode = item.reader("data-color");
         navigator.clipboard
           .writeText(colorCode)
           .then(() => showToast(`Copied: ${colorCode}`))
@@ -171,6 +168,7 @@ const displayColors = (colors) => {
 // function to display all the tags with pagination
 let allTags = [];
 let displayedTags = 0;
+let tagsPerPage = 20;
 
 const displayTags = (tags) => {
   const tagsContainer = document.querySelector(".AI-tags-container");
@@ -198,17 +196,17 @@ const displayTags = (tags) => {
       displayedTags,
       displayedTags + tagsPerPage
     );
-    displayTags += tagsToShow.length;
+    displayedTags += tagsToShow.length;
 
     const tagsHTML = tagsToShow
       .map(
         ({ tag: { en } }) => `
-
-      <div class="result-item">
-      <p>${en}</p>
-      </div>
-    
-    `
+  
+        <div class="result-item">
+        <p>${en}</p>
+        </div>
+      
+      `
       )
       .join("");
 
